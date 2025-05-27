f42799cd91bb7f7a82bc561abfb784d238d6b281
Muhammad-Irtiza10 <muhammadirtiza30@gmail.com>
2025-05-27 04:25:47 -0700

    Catalog SKills Extraction (Mass Comm Digital media)

--- skillgap_project/settings.py
+++ skillgap_project/settings.py
@@ -74,15 +74,16 @@ WSGI_APPLICATION = "skillgap_project.wsgi.application"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE':   'django.db.backends.postgresql',
-        'NAME':     'skillgap_prod',
-        'USER':     'rashid',
-        'PASSWORD': 'root',
-        'HOST':     'localhost',
-        'PORT':     '5432',
-    }
+  'default': {
+    'ENGINE':   'django.db.backends.postgresql',
+    'NAME':     'skillgap_prod',
+    'USER':     'postgres',
+    'PASSWORD': 'root',       # or whatever you set at install
+    'HOST':     'localhost',
+    'PORT':     '5432',
+  }
 }
+
 
 
 # Password validation
